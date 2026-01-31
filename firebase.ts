1319ea60ff0861532f5b3a93d6dadd396f551a70
pp1rstt-hub <pp1rstt@gmail.com>
2026-01-31 18:42:35 +0200

    final fix

--- firebase.ts
+++ firebase.ts
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB-YourActualKeyбудеТут", 
+    apiKey: "AIzaSyB-YourActualKey", 
     authDomain: "mint-chat.firebaseapp.com",
     projectId: "mint-chat",
     storageBucket: "mint-chat.appspot.com",
@@ -11,9 +11,6 @@ const firebaseConfig = {
     appId: "1:1064372554733:web:7f6f598282365e69e06876"
 };
 
-// Це щоб не було помилок при перевантаженні сторінки
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-export { db, storage };
+export const db = getFirestore(app);
+export const storage = getStorage(app);
